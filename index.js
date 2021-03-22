21a7c3e7a2a26518c40aaa98ad6e192c745ce491
calvinhyacinth@gmail.com <calvinhyacinth@gmail.com>
2021-03-22 12:10:07 +0100

    Add exisiting file

---
+++ index.js
@@ -0,0 +1,12 @@
+const myInformation = require('./information.js');
+myInformation();
+
+var cowsay = require("cowsay");
+
+console.log(cowsay.say({
+	text : "Hello I'm Calvin from the Reims Campus!",
+	e : "oO",
+	T : "U "
+}));
+
+
---
+++ information.js
@@ -0,0 +1,12 @@
+let name ='Calvin Hyacinth';
+
+let city = 'Reims';
+
+const myInformation = () => {
+    console.log(`Hello I'm ${name} from the ${city} Campus!`);
+};
+ 
+module.exports = myInformation;
+
+const myInfo = { name:"Calvin Hyacinth", city:"Reims"};
+
